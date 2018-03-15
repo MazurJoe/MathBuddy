e80e4083da382cdea2eefe7a5e266dff6c8f9ffc
PaulNguyen2518 <35715419+PaulNguyen2518@users.noreply.github.com>
2018-03-15 16:51:22 -0500

    Fixed random int number generator
    Number generator method only generated one random number when called. Methods can now be imported to other classes to generate a random number within a given range. Double number generator still needs to be fixed; number generated has no decimal value, i.e. it produces an integer.

--- NumberGenerator.java
+++ NumberGenerator.java
@@ -1,23 +1,37 @@
 /**
- * created by Joseph Mazur
- * @since 03-12-2018
-*/
+ * @author Joseph Mazur
+ * @author Paul John Nguyen
+ * @since 03-15-2018
+ * @version
+ */
 package mathbuddy;
 
 import java.util.Random;
+
 public class NumberGenerator
 {
-    //this method takes two parameters as a range from which to generate numbers from, and returns an integer thats within the specified range
+    final static Random GENERATOR = new Random(System.currentTimeMillis());
+    
+    /**
+     * this method takes two parameters as a range from which to generate numbers from, and returns an integer thats within the specified range
+     * @param numLow lowest number of range
+     * @param numHigh highest number of range
+     * @return integer within range
+     */
     public static int intNumGen(int numLow, int numHigh)
-    {        
-        Random generator = new Random(System.currentTimeMillis());
-        return generator.nextInt(numHigh-numLow+1)+numLow;
+    {
+        return GENERATOR.nextInt(numHigh-numLow+1)+numLow;
     }
     
-    //this method returns a double from a given range up to a desired number of decimal places. 
+    /**
+     * this method returns a double from a given range up to a desired number of decimal places. 
+     * @param numLow lowest number of range
+     * @param numHigh highest number of range
+     * @param numOfDecimalPlaces number of decimal places
+     * @return double within range
+     */
     public static double doubleNumGen(int numLow, int numHigh, int numOfDecimalPlaces)
     {
-        Random generator = new Random(System.currentTimeMillis());
-        return (generator.nextInt((numHigh-numLow)*10^numOfDecimalPlaces +1)+numLow*10^numOfDecimalPlaces)/(10^numOfDecimalPlaces);
+        return (GENERATOR.nextInt((numHigh-numLow)*10^numOfDecimalPlaces +1)+numLow*10^numOfDecimalPlaces)/(10^numOfDecimalPlaces);
     }
 }
