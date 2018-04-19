b2237e383205ae8e5e378849dc7e0dadb791eca6
MazurJoe <35609388+MazurJoe@users.noreply.github.com>
2018-04-19 09:52:15 -0500

    Add files via upload

--- MathBuddy/src/mathbuddy/Comparator.java
+++ MathBuddy/src/mathbuddy/Comparator.java
@@ -6,9 +6,9 @@ package mathbuddy;
 public class Comparator 
 { 
     //this method compares the diference of two doubles, and if the result is <= 1/1000, it returns true. 
-    public boolean numComparator(double userAnswer, double correctAnswer)
+    public static boolean numComparator(double userAnswer, double correctAnswer)
     {
-        if(Math.abs(userAnswer-correctAnswer)<= .001)
+        if(Math.abs(userAnswer-correctAnswer)<= .0001)
             return true;
         return false;
     }    
